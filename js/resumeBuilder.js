4df444f36ab5217e0410e62201b25d05fccb875d
Ryan Minniear <rjminniear@gmail.com>
2017-02-10 20:17:58 -0800

    Add work experience and education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -14,6 +14,22 @@ var bio = {
 	"skills": ["Java", "Python", "C", "Git/GitHub"]
 };
 
+var work = {
+	"employer" : "IBM",
+	"title" : "Software Engineer",
+	"dates": "October 2006 - May 2014",
+	"location" : "Folsom",
+	"description" : "Software Development work across multiple products and teams"
+};
+
+var education = {
+	"school" : "UC Davis",
+	"degree" : "Bachelor of Science",
+	"dates" : "2002-2006",
+	"location" : "Davis, CA",
+	"major" : "Computer Science"
+};
+
 /* Return list of HTML-formatted skills */
 function getFormattedSkills() {
 	return bio.skills.map(function(skill) {
@@ -21,26 +37,38 @@ function getFormattedSkills() {
 	}).join("");
 };
 
-formattedName = HTMLheaderName.replace("%data%", bio.name);
-formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-formattedPic = HTMLbioPic.replace("%data%", bio.picture_URL);
-formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome_message);
-formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
-formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
-formattedTwitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
-formattedGithub = HTMLgithub.replace("%data%", bio.contact.github);
-formattedBlog = HTMLblog.replace("%data%", bio.contact.blog);
-formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedPic = HTMLbioPic.replace("%data%", bio.picture_URL);
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome_message);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contact.github);
+var formattedBlog = HTMLblog.replace("%data%", bio.contact.blog);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
 
+var formattedEmployer = HTMLworkEmployer.replace("%data%", work.employer)
+var formattedTitle = HTMLworkTitle.replace("%data%", work.title);
+var formattedDates = HTMLworkDates.replace("%data%", work.dates);
+var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.location);
+var formattedDescription = HTMLworkDescription.replace("%data%", work.description);
+
+var formattedSchool = HTMLschoolName.replace("%data%", education["school"]);
+var formattedDegree = HTMLschoolDegree.replace("%data%", education["degree"]);
+var formattedSchoolDates = HTMLschoolDates.replace("%data%", education["dates"]);
+var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education["location"]);
+var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education["major"]);
 
 /* Header information */
 $("#header").prepend(formattedWelcome);
-// Note: HTMLskillsStart contains the "skills" id referenced in the line below
 $("#header").prepend(HTMLskillsStart);
+// Note: The line added the ul with the "skills" id referenced here
 $("#skills").append(getFormattedSkills());
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 $("#header").prepend(formattedPic);
+
 /* Contact information */
 $("#topContacts").append(formattedMobile);
 $("#topContacts").append(formattedEmail);
@@ -48,3 +76,17 @@ $("#topContacts").append(formattedTwitter);
 $("#topContacts").append(formattedGithub);
 $("#topContacts").append(formattedBlog);
 $("#topContacts").append(formattedLocation);
+
+/* Employment history */
+$("#workExperience").append(HTMLworkStart);
+$(".work-entry").append(formattedEmployer + formattedTitle);
+$(".work-entry").append(formattedDates);
+$(".work-entry").append(formattedWorkLocation);
+$(".work-entry").append(formattedDescription);
+
+/* Education */
+$("#education").append(HTMLschoolStart);
+$(".education-entry").append(formattedSchool + formattedDegree);
+$(".education-entry").append(formattedSchoolDates);
+$(".education-entry").append(formattedSchoolLocation);
+$(".education-entry").append(formattedSchoolMajor);
